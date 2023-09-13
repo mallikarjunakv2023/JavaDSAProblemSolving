fb09f3bb82ab9eb9318cb1ac7b94006e5bc21374
mallikarjunakv <mallikarjunakv@dpworld.com>
2023-09-13 18:26:24 +0530

    String Programs 2

--- src/main/java/org/java/programs/Add2BinaryStrings.java
+++ src/main/java/org/java/programs/Add2BinaryStrings.java
@@ -1,5 +1,13 @@
 package org.java.programs;
+/*
+logic : 2 binary strings contains only 0 and 1, ASCII value of 1 isc 49 and 0 is 48,
+use this difference 49-48 = 1 to build a logic for binary addition.
+Take 2 pointers i and j of length str1 and str2, and carry with init value as 0,
+put a while loop which breaks when i and j < 0 and carry is != 1. check if i and j >=0
+and compute carry. Append the modulus 2 of carry to string builder. reverse and the sb
+to ge the result
 
+ */
 public class Add2BinaryStrings {
     public String addBinary(String a, String b) {
         StringBuilder sb = new StringBuilder();
