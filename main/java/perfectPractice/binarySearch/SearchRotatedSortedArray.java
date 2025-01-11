35a68ae84ab4d4c00dc124718f8bda4c84441b8a
mkv <mkv@mimecast.com>
2025-01-11 21:46:23 +0530

    LinkedList and BS solutions

---
+++ main/java/perfectPractice/binarySearch/SearchRotatedSortedArray.java
@@ -0,0 +1,44 @@
+package perfectPractice.binarySearch;
+
+public class SearchRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        //put a binary seach - calculate mid
+        //find if the left part is sorted, check if low <= mid to confirm sorting, assign low and high
+        //if else,
+        /*
+        Example 1:
+
+            Input: nums = [4,5,6,7,0,1,2], target = 0
+            Output: 4
+            Example 2:
+
+            Input: nums = [4,5,6,7,0,1,2], target = 3
+            Output: -1
+
+         */
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[low] <= nums[mid]) { //tricky logic, dry run again
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
---
+++ main/java/perfectPractice/linkedList/MiddleOfLL.java
@@ -0,0 +1,34 @@
+package perfectPractice.linkedList;
+
+import programs75.ListNode;
+
+public class MiddleOfLL {
+    public ListNode middleNode(ListNode head) {
+        ListNode temp = head;
+        int count = 0;
+        while(temp != null){
+            count++;
+            temp = temp.next;
+        }
+        int mid = (count/2) + 1;
+        System.out.println(mid);
+        temp = head;
+        while (mid > 1) {
+            mid--;
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public ListNode middleNode1(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+
+    }
+}
